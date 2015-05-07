0618373911044699354c98348d9b2310508c1435
oquintansocampo <oquintansocampo@atanasoft06.danielcastelao.org>
2015-05-07 08:48:55 +0200

    Modelo de Circulo

---
+++ src/superficies/Circulo.java
@@ -0,0 +1,42 @@
+package superficies;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author oquintansocampo
+ */
+public class Circulo {
+
+    private float radio;
+    private float area;
+
+    public Circulo() {
+    }
+
+    public Circulo(float radio, float areaC) {
+        this.radio = radio;
+        this.area = areaC;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float getAreaC() {
+        return area;
+    }
+
+    public void setAreaC(float areaC) {
+        this.area = areaC;
+    }
+
+}
