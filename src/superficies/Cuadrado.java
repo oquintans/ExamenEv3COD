0495b460cdea081e5106624b8c4f282985862662
oquintansocampo <oquintansocampo@atanasoft06.danielcastelao.org>
2015-05-07 08:49:51 +0200

    Modelo Cuadrado

---
+++ src/superficies/Cuadrado.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superficies;
+
+/**
+ *
+ * @author oquintansocampo
+ */
+public class Cuadrado {
+
+    private float lado;
+    private float area;
+
+    public Cuadrado() {
+    }
+
+    public Cuadrado(float lado, float area) {
+        this.lado = lado;
+        this.area = area;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+    
+}
